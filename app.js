3b56086a41fcd68ea4baf5ac54780385081962bd
Jeet Mukherjee <jeetmukherjee100@gmail.com>
2025-06-06 17:04:14 +0530

    fix env vars

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-import bodyParser from 'body-parser';
-import express from 'express';
+import "dotenv/config";
 
-import eventRoutes from './routes/events.js';
+import bodyParser from "body-parser";
+import express from "express";
+
+import eventRoutes from "./routes/events.js";
 
 const app = express();
 
